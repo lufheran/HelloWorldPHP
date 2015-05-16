4d3260c2292b03a0112d1560bdd664d3ba08f612
Luis Hernandez <lufheran@gmail.com>
2015-05-16 14:55:28 -0500

    Hello world mal escrito en php

---
+++ Index.php
@@ -0,0 +1,11 @@
+<?php  $language="PHP";?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+</head>
+<body>
+  <h1>Mejorando.la</h1>
+  <?php echo "Aprediedno <strong>$language</strong>";?>
+</body>
+</html>
