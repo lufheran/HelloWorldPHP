8c634ed02574796a6962a48f338a403af512e47a
Luis Hernandez <lufheran@gmail.com>
2015-05-18 16:23:49 -0500

    Se creo el frontend controller y se organizo la estructura

--- Index.php
+++ Index.php
@@ -1,9 +1,25 @@
 <?php
 
-	require 'config.php';
-	require 'helpers.php';
-    $confidencial ="un texto cualquiera";
-    $language="PHP";
-    $title = "Mejorandola";
-    //Llamar una funcion
-    view('home',compact('language','title'));
+    /**
+    ** El frontend controller se encarga de configurar nuestra aplicacion
+     */
+    require 'config.php';
+    require 'helpers.php';
+
+    //Llamar a los controladores individuales
+    //index.php
+    if(empty($_GET['url']))
+    {
+        require 'controllers/home.php';
+    }
+    //index.php?url=contactos
+    elseif($_GET['url'] == 'contactos')
+    {
+        require 'controllers/contactos.php';
+    }
+    //Mensaje de error
+    else
+    {
+        header('HTTP/1.0 404 Not Found');
+        exit("Pagina no encontrada");
+    }
---
+++ controllers/home.php
@@ -0,0 +1,7 @@
+<?php
+
+    $confidencial ="un texto cualquiera";
+    $language="PHP";
+    $title = "Mejorandola";
+    //Llamar una funcion
+    view('home',compact('language','title'));
