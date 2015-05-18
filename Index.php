6af1479c5d09230a85078490f415ee2e524275a6
Luis Hernandez <lufheran@gmail.com>
2015-05-18 17:51:34 -0500

    Refactorizando frontend

--- Index.php
+++ Index.php
@@ -6,20 +6,10 @@
     require 'config.php';
     require 'helpers.php';
 
-    //Llamar a los controladores individuales
-    //index.php
+    //Llamar al controlador indicado.
+
     if(empty($_GET['url']))
     {
-        require 'controllers/home.php';
+        $_GET['url'] = "home";
     }
-    //index.php?url=contactos
-    elseif($_GET['url'] == 'contactos')
-    {
-        require 'controllers/contactos.php';
-    }
-    //Mensaje de error
-    else
-    {
-        header('HTTP/1.0 404 Not Found');
-        exit("Pagina no encontrada");
-    }
+    controller($_GET['url']);
--- helpers.php
+++ helpers.php
@@ -6,3 +6,17 @@
         extract($var);
     	require "view/$template.tpl.php";
     }
+
+    function controller($name)
+    {
+        $file = "controllers/$name.php";
+        if(file_exists($file))
+        {
+            require $file;
+        }
+        else
+        {
+            header('HTTP/1.0 Not Found');
+            exit('pagina no encontrada');
+        }
+    }
